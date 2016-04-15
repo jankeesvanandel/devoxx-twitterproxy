24e3bf7e63af39025898d2571537afd0c15381b4
Jan-Kees van Andel <jankeesvanandel@gmail.com>
2016-04-15 14:53:04 +0200

    Added support for Devoxx FR

--- src/main/scala/DevoxxTwitterProxy.scala
+++ src/main/scala/DevoxxTwitterProxy.scala
@@ -1,24 +1,17 @@
-import java.io.{InputStreamReader, BufferedReader}
-
 import akka.actor.ActorSystem
-import akka.event.{Logging, LoggingAdapter}
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.headers.{`Access-Control-Allow-Origin`, HttpOriginRange}
-import akka.http.scaladsl.{HttpExt, Http}
+import akka.event.Logging
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.{HttpOriginRange, `Access-Control-Allow-Origin`}
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import akka.stream.{ActorMaterializer, Materializer}
-import com.typesafe.config.{Config, ConfigFactory}
-import spray.json.DefaultJsonProtocol
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import spray.json.{DefaultJsonProtocol, _}
 import twitter4j._
-import twitter4j.auth.{AccessToken, RequestToken}
 
 import scala.collection.JavaConverters._
-import scala.concurrent.ExecutionContextExecutor
+import scala.collection.mutable
 import scala.util.{Failure, Success, Try}
-import spray.json._
-import DefaultJsonProtocol._
 
 case class Tweet(id: Long, from: String, profileImageUrl: String, message: String)
 case class ErrorMessage(message: String)
@@ -36,31 +29,34 @@ trait DevoxxTwitterProxyService extends App with DefaultJsonProtocol {
 
   val twitter = Try(new TwitterFactory().getInstance())
 
-  var cache: (Long, List[Tweet])
+  var cache: mutable.Map[String, (Long, List[Tweet])]
   val cacheTime: Long
 
-  def fetchWithCache(sinceId: Long): Try[List[Tweet]] = {
-    if (cache._1 + cacheTime <= System.currentTimeMillis()) {
-      val tweets = fetchTweetsForDevoxx(sinceId)
+  def fetchWithCache(event: String, sinceId: Long): Try[List[Tweet]] = {
+    val eventCache = cache(event)
+    if (eventCache._1 + cacheTime <= System.currentTimeMillis()) {
+      val tweets = fetchTweets(event, sinceId)
       logger.debug("Retrieving tweets from Twitter")
-      tweets.map{ tweetsList =>
+      tweets.map { tweetsList =>
         logger.debug(s"Putting ${tweetsList.size} in cache")
-        cache = (System.currentTimeMillis(), tweetsList)
+        cache.put(event, (System.currentTimeMillis(), tweetsList))
         tweetsList
       }
     } else {
       logger.debug("Retrieving tweets from cache")
-      Success(cache._2)
+      Success(eventCache._2)
     }
   }
 
-  def fetchTweetsForDevoxx(sinceId: Long): Try[List[Tweet]] = {
+  def fetchTweets(event: String, sinceId: Long): Try[List[Tweet]] = {
     twitter.map { t =>
-      val query = new Query("#devoxx OR #devoxx15 OR #devoxx2015 OR @devoxx")
+      val yearLong = DateTime(System.currentTimeMillis()).year
+      val yearShort = yearLong - 2000
+      val query = new Query(s"#$event OR #$event$yearShort OR #$event$yearLong OR @$event")
       query.setCount(100)
       query.setSinceId(sinceId)
       t.search(query).getTweets.asScala.toList
-    }.map { tweets =>
+    }.map { (tweets: List[Status]) =>
       tweets.map { tweet =>
         Tweet(
           tweet.getId,
@@ -73,13 +69,15 @@ trait DevoxxTwitterProxyService extends App with DefaultJsonProtocol {
 
   val routes = {
     logRequestResult("akka-http-microservice") {
-      pathPrefix("tweets" / "devoxx" / LongNumber) { sinceId =>
-        get {
-          respondWithHeader(`Access-Control-Allow-Origin`.forRange(HttpOriginRange.*)) {
-            complete {
-              fetchWithCache(sinceId) match {
-                case Success(tweets) => StatusCodes.OK -> tweets.toJson.compactPrint
-                case Failure(exception) => StatusCodes.BadRequest -> exception.getMessage
+      pathPrefix("tweets" / ".*{1,20}".r) { event =>
+        path(LongNumber) { sinceId =>
+          get {
+            respondWithHeader(`Access-Control-Allow-Origin`.forRange(HttpOriginRange.*)) {
+              complete {
+                fetchWithCache(event, sinceId) match {
+                  case Success(tweets) => StatusCodes.OK -> tweets.toJson.compactPrint
+                  case Failure(exception) => StatusCodes.BadRequest -> exception.getMessage
+                }
               }
             }
           }
@@ -92,7 +90,7 @@ trait DevoxxTwitterProxyService extends App with DefaultJsonProtocol {
 
 object DevoxxTwitterProxy extends DevoxxTwitterProxyService {
 
-  var cache: (Long, List[Tweet]) = (0, Nil)
+  var cache: mutable.Map[String, (Long, List[Tweet])] = mutable.Map.empty.withDefaultValue((0L, Nil))
   val cacheTime: Long = 10000 // 10 seconds cache time
 
   Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
